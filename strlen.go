d28e3c9c95190d5d4dbab0f1c25e37d2ca34de9b
Travis Simon <tsimon@gmail.com>
2016-02-12 22:37:45 +1100

    Updating strlen to use remnant client

--- strlen.go
+++ strlen.go
@@ -1,47 +1,104 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
 	"unicode/utf8"
+
+	"github.com/travissimon/remnant/client"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
+func swaggerHandler(w http.ResponseWriter, r *http.Request) {
 	respJson := `{
-    "kind": "pathList",
-    "items": [
-        "/v1",
-        "/v2",
-        "/healthz"
+    "swagger": "2.0",
+    "info": {
+        "description": "This finds the length of a string. It's AWESOME!\n",
+        "version": "1.0.0",
+        "title": "strlen",
+        "contact": {
+            "name": "Travis"
+            "email": "travis.simon@nicta.com.au"
+        },
+        "license": {
+            "name": "GPL 3.0",
+            "url": "http://www.gnu.org/licenses/gpl-3.0.en.html"
+        }
+    },
+    "host": "strlen",
+    "basePath": "/v1",
+    "schemes": [
+        "http"
     ],
-    "documentation": "http://github.com/travissimon/microservices/strlen/README.md",
-    "contact": "travis.simon@nicta.com.au"
+    "consumes": [
+        "application/json"
+    ],
+    "produces": [
+        "application/json"
+    ],
+    "paths": {
+        "/strlen/{string}": {
+            "get": {
+                "tags": [
+                    "string"
+                ],
+                "summary": "Find the length of a string",
+                "description": "Returns length of a string",
+                "produces": [
+                    "application/json"
+                ],
+                "parameters": [
+                    {
+                        "in": "path",
+                        "name": "string",
+                        "description": "string whose length is unknown",
+                        "required": true,
+                        "type": "string"
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "successful operation",
+                        "schema": {
+                            "type": "integer",
+                            "format": "int64"
+                        }
+                    }
+                }
+            }
+        },
+        "/healthz": {
+            "get": {
+                "tags": [
+                    "monitoring"
+                ],
+                "summary": "Heartbeat response",
+                "description": "Provides a monitoring service with proof-of-life",
+                "operationId": "isAlive",
+                "responses": {
+                    "default": {
+                        "description": "successful operation"
+                    }
+                }
+            }
+        }
+    }
 }`
 
 	fmt.Fprintf(w, respJson)
 }
 
-func defaultStrLenHandler(w http.ResponseWriter, r *http.Request) {
-	respJson := `{
-    "kind": "pathList",
-    "items": [
-        "/strlen",
-    ],
-}`
+func strLenHandler(w http.ResponseWriter, r *http.Request) {
+	cl, err := client.NewRemnantClient("http://localhost:7777/", r)
+	defer cl.EndSpan()
 
-	fmt.Fprintf(w, respJson)
-}
-
-func strLenV1Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("request: %s\n", r.URL.Path)
-	s := string(r.URL.Path[len("/v1/len/"):])
-	fmt.Fprintf(w, "%d", utf8.RuneCountInString(s))
-}
-
-func strLenV2Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("request: %s\n", r.URL.Path)
-	s := string(r.URL.Path[len("/v2/len/"):])
+	fmt.Printf("%s request: %s\n", time.Now().UTC().Format(time.RFC3339), r.URL.Path)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		return
+	}
+	s := string(r.URL.Path[len("/v1/strlen/"):])
 	fmt.Fprintf(w, "%d", utf8.RuneCountInString(s))
 }
 
@@ -51,13 +108,14 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", defaultHandler)
-	http.HandleFunc("/v1/", defaultStrLenHandler)
-	http.HandleFunc("/v2/", defaultStrLenHandler)
-	http.HandleFunc("/v1/len/", strLenV1Handler)
-	http.HandleFunc("/v2/len/", strLenV2Handler)
+	var port = flag.String("port", "8080", "Define what TCP port to bind to")
+	flag.Parse()
+
+	http.HandleFunc("/", swaggerHandler)
+	http.HandleFunc("/swagger.json", swaggerHandler)
+	http.HandleFunc("/v1/strlen/", strLenHandler)
 	http.HandleFunc("/healthz", healthzHandler)
 
-	fmt.Printf("Starting server on port 8080\n")
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("Starting server on port %s\n", *port)
+	http.ListenAndServe(":"+*port, nil)
 }
